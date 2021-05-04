0167b38c57bded0155520f15b0b9f6a6bf21aed8
nmartinezgross <ngross@frba.utn.edu.ar>
2021-05-03 21:45:28 -0300

    Modificaciones dentro de los Makefile

--- 01-Interfaces & Makefile - Temperaturas/TablasDeConversion.c
+++ 01-Interfaces & Makefile - Temperaturas/TablasDeConversion.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "Conversion.h"
 
-int maint (void){
+int main()
+{
         const int LOWER_F = 0;
         const int UPPER_F = 300;
         const int STEP_F = 20;
@@ -11,9 +12,9 @@ int maint (void){
 
         printf("Tabla de Conversion - Farenheit a Celsius:\n");
         for(int fahr = LOWER_F; fahr <= UPPER_F; fahr = fahr + STEP_F)
-        printf("%3d %6.1f\n", fahr, Celsius(fahr) );
+        printf("%5d %6.1f\n", fahr, Celsius(fahr) );
 
-        printf("Tabla de Conversion - Celsius a Farenheit:\n");
-        for(int cels = LOWER_C; cels <= UPPER_C; cels = cels + STEP_C)
-        printf("%5.1f %9.0f\n", cels, Farenheit(cels));
-}
+        printf("\nTabla de Conversion - Celsius a Farenheit:\n");
+        for(double cels = LOWER_C; cels <= UPPER_C; cels = cels + STEP_C)
+                printf("%5.1f %6.0f\n", cels, Farenheit(cels));
+}        
